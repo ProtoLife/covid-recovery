1ea6239f24b3851d386c59495287c9c2545ced62
Norman Packard <nhpackard@mac.com>
2020-05-21 18:58:26 -0700

    tweak covid-data-explore-jhu.ipynb to use local JH data from submodule

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,9 +1,11 @@
 echo "updating covid-19-owid"
 git submodule update --remote --merge covid-19-owid
 echo "done with covid-19-owid"
+echo "--------------------------------------------------"
 echo "updating covid-policy-tracker"
 git submodule update --remote --merge covid-policy-tracker
 echo "done with covid-policy-tracker"
+echo "--------------------------------------------------"
 echo "updating covid-19-JH"
 git submodule update --remote --merge covid-19-JH
 echo "done with covid-19-JH"
